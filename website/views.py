50796edb362ea6ee20c7010540c2e8774b4c0923
Li Gao <ligao0807@gmail.com>
2021-05-13 20:15:51 -0400

    make a item page

--- website/views.py
+++ website/views.py
@@ -20,11 +20,13 @@ def market():
     if request.method == 'POST':
         food_item = request.form.get('searchfood')
         # don't do error checks, some boxes can be empty
-        searchresults = db.session.execute('SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE U.id = S.user_id AND S.food = :val', {'val': food_item})
-        #searchresults = Seller.query.filter_by(food=food_item) #okay so search results need to give full table
+        searchresults = db.session.execute(
+            'SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE U.id = S.user_id AND S.food = :val', {'val': food_item})
+        # searchresults = Seller.query.filter_by(food=food_item) #okay so search results need to give full table
         return render_template("market.html", user=current_user, searchresults=searchresults)
 
-    result = db.session.execute('SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE S.user_id = U.id')
+    result = db.session.execute(
+        'SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE S.user_id = U.id')
     my_stuff = Seller.query.filter_by(user_id=current_user.id)
     return render_template("market.html", user=current_user, seller=result, my_stuff=my_stuff)
 
@@ -48,7 +50,7 @@ def home():
             flash('please give a description', category='error')
         elif len(expiration) < 1:
             flash('please give an expiration date', category='error')
-        
+
         else:
             new_seller = Seller(food=food, price=price, description=description,
                                 expiration=expiration, user_id=current_user.id)
@@ -60,20 +62,21 @@ def home():
     # so we can check if authenticated
     return render_template("home.html", user=current_user)
 
+
 @views.route('/purchase/<int:id>')
 def purchase(id):
-    #here i could grab all the seller and buyer info
-    seller_item = db.session.execute('SELECT S.id, U.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE S.id = :val', {'val': id})
-    #okay, this is so I can add the item to the transaction table
-    #and removing it from the market table
-    #I could manage that in the market backend b/c ill match seller id with 
-    #item's seller id and choose to not display it, along with user's own item
-    #okay, so I take from this table the specific places and make them variables,
-    #then add those variables into a database called transactions
-    # i should at least have buyer's id, seller's id, item's id   
+    # here i could grab all the seller and buyer info
+    seller_item = db.session.execute(
+        'SELECT S.id, U.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE S.id = :val', {'val': id})
+    # okay, this is so I can add the item to the transaction table
+    # and removing it from the market table
+    # I could manage that in the market backend b/c ill match seller id with
+    # item's seller id and choose to not display it, along with user's own item
+    # okay, so I take from this table the specific places and make them variables,
+    # then add those variables into a database called transactions
+    # i should at least have buyer's id, seller's id, item's id
 
     return render_template("buy.html", user=current_user, seller_item=seller_item)
-
 
 
 @views.route('/delete/<int:id>')
@@ -92,3 +95,20 @@ def delete(id):
     else:
         flash('You can only delete your posted items', category='error')
         return redirect(url_for('views.market'))
+
+
+@views.route('/items', methods=['GET', 'POST'])
+@login_required
+def items():
+    if request.method == 'POST':
+        food_item = request.form.get('searchfood')
+        # don't do error checks, some boxes can be empty
+        searchresults = db.session.execute(
+            'SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE U.id = S.user_id AND S.food = :val', {'val': food_item})
+        # searchresults = Seller.query.filter_by(food=food_item) #okay so search results need to give full table
+        return render_template("items.html", user=current_user, searchresults=searchresults)
+
+    result = db.session.execute(
+        'SELECT S.id, S.food, S.price, S.description, S.expiration, U.user_name, U.address, U.zipcode, U.phone FROM Seller AS S JOIN User AS U WHERE S.user_id = U.id')
+    my_stuff = Seller.query.filter_by(user_id=current_user.id)
+    return render_template("items.html", user=current_user, seller=result, my_stuff=my_stuff)
