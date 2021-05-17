d8280ee23cf026698dae10dc2e11f43861fc230d
Warren-source <55956652+Warren-source@users.noreply.github.com>
2021-05-16 20:16:08 -0400

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from .models import User, Buyer, Seller, Location
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -34,17 +34,18 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+#this logs the user out
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-####note to self, adding food page with adding food front end
+#this displays and tskes posts from the sign up page
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
     #if request method is a post, set a bunch of variables
-    #to the fields
+    #to take from each of the fields
     if request.method =='POST':
         user_name = request.form.get('userName')
         password1 = request.form.get('password1')
@@ -52,11 +53,16 @@ def sign_up():
         address = request.form.get('address')
         zipcode = request.form.get('zipcode')
         phone = request.form.get('phone')
+        card = request.form.get('card')
+        license = request.form.get('license')
+        town = request.form.get('town')
+        state = request.form.get('state')
         #this is just to check if the info submitted is all there
         #and the passwords match
         #we must first check if the user already exists
         user = User.query.filter_by(user_name=user_name).first()
 
+        #and this is just error checking
         if user:
             flash('This username is already taken', category='error')
         elif len(user_name) < 1:
@@ -67,6 +73,10 @@ def sign_up():
             flash('Passwords must match', category='error')
         elif len(address) < 5:
             flash('Please give an address', category='error')
+        elif len(town) < 1:
+            flash('Please give a town', category='error')
+        elif len(state) < 1:
+            flash('Please give a state', category='error')
         elif len(zipcode) < 5:
             flash('Please give a zip code', category='error')
         elif len(phone) < 10:
@@ -77,8 +87,27 @@ def sign_up():
                             address=address, zipcode=zipcode, phone=phone)
             db.session.add(new_user)
             db.session.commit()
+            #now we have to query to see if the zipcode already exists in location table
+            location_exists = Location.query.filter_by(zipcode=zipcode).first()
+            if(location_exists == None):
+                new_location = Location(town=town, state=state, zipcode=zipcode)
+                db.session.add(new_location)
+                db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created. Welcome', category='success')
+            #give them a place in the buyer and/or seller table if they fill out
+            #the proper forms 
+            if len(card) > 0:
+                new_buyer = Buyer(card=card, user_name=current_user.user_name)
+                db.session.add(new_buyer)
+                db.session.commit()
+                flash('Your account has buyer priviledges', category='success')
+            if len(license) > 0:
+                new_seller = Seller(license=license, user_name=current_user.user_name)
+                db.session.add(new_seller)
+                db.session.commit()
+                flash('Your account has seller priviledges', category='success')
+
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
