ac5883da85a64da0cbc7cc51c4e03591e2cc6ec1
Warren-source <55956652+Warren-source@users.noreply.github.com>
2021-05-16 20:18:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-#this is where we run the app, to run. type on command line python3 main.py    
+#this is where we run the app. To run, type on command line python3 main.py    
